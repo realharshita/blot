659b96f2952a29c25c205bc96226491aed65d255
realharshita <slayyharshita@gmail.com>
2024-06-29 01:02:16 +0530

    car in blot

--- project.js
+++ project.js
@@ -1,58 +1,23 @@
-const width = 200; 
-const height = 200; 
-
-setDocDimensions(width, height);
-
-const hPolylines = [
-  [[0, 0], [0, 100]],
-  [[0, 50], [50, 50]],
-  [[50, 0], [50, 100]]
-];
-
-const aPolylines = [
-  [[0, 100], [25, 0]],
-  [[25, 0], [50, 100]],
-  [[12.5, 50], [37.5, 50]]
-];
-
-const rPolylines = [
-  [[0, 0], [0, 100]],
-  [[0, 50], [50, 50]],
-  [[50, 50], [50, 75]],
-  [[50, 75], [25, 100]]
-];
-
-const sPolylines = [
-  [[0, 75], [50, 75]],
-  [[0, 25], [50, 25]],
-  [[0, 75], [0, 50]],
-  [[50, 50], [50, 25]],
-  [[50, 50], [0, 25]]
-];
-
-const tPolylines = [
-  [[0, 100], [50, 100]],
-  [[25, 100], [25, 0]]
-];
-
-const iPolylines = [
-  [[25, 100], [25, 0]]
-];
-bt.translate(hPolylines, [0, 0]);
-bt.translate(aPolylines, [60, 0]);
-bt.translate(rPolylines, [120, 0]);
-bt.translate(sPolylines, [180, 0]);
-bt.translate(tPolylines, [240, 0]);
-bt.translate(iPolylines, [280, 0]);
+const max_x = 723;
+const max_y = 194;
+setDocDimensions(max_x,max_y);
 
 
-const harshitaPolylines = [
-  ...hPolylines,
-  ...aPolylines,
-  ...rPolylines,
-  ...sPolylines,
-  ...tPolylines,
-  ...iPolylines
-];
 
-drawLines(harshitaPolylines);
+const t = new bt.Turtle()
+t.left(90)
+t.forward(88)
+t.right(90)
+t.forward(101)
+t.right(180)
+t.forward(101)
+t.right(120)
+t.forward(15)
+t.right(50)
+t.forward(145)
+t.right(10)
+t.forward(573)
+
+
+
+drawLines(t.lines())
